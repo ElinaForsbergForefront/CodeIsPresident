c89195296daa75a6b7bbab3fa125ad8b9a507f56
Elina Forsberg <elina.forsberg@sveaskog.se>
2025-09-16 08:15:32 +0200

    solid version :)

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,20 +1,66 @@
-﻿
-for (int i = 1; i < 101; i++)
+﻿public interface IFizzBuzzRule
 {
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-        Console.WriteLine("FizzBuzz");
+    bool AppliesTo(int number);
+    string GetValue();
+}
+
+public class FizzRule : IFizzBuzzRule
+{
+    public bool AppliesTo(int number) 
+    { 
+        if (number % 3 == 0) return true; 
+        return false; 
     }
-    else if (i % 3 == 0)
-    {
-        Console.WriteLine("Fizz");
+    public string GetValue() => "Fizz";
+}
+
+public class BuzzRule : IFizzBuzzRule
+{
+    public bool AppliesTo(int number) 
+    { 
+        if (number % 5 == 0) return true; 
+        return false; 
     }
-    else if (i % 5 == 0)
+    public string GetValue() => "Buzz";
+}
+
+public class FizzBuzzRule : IFizzBuzzRule
+{
+    public bool AppliesTo(int number)
     {
-        Console.WriteLine("Buzz");
+        if (number % 3 == 0 && number % 5 == 0) return true;
+        return false;
     }
-    else
+
+    public string GetValue() => "Fizzbuzz";
+}
+
+public class FizzBuzzCalculator
+{
+    private readonly IEnumerable<IFizzBuzzRule> _rules;
+    public FizzBuzzCalculator(IEnumerable<IFizzBuzzRule> rules) => _rules = rules;
+
+    public string Calculate(int number)
     {
-        Console.WriteLine(i);
+        var result = _rules.FirstOrDefault(r => r.AppliesTo(number));
+
+        return result != null ? result.GetValue() : number.ToString();
     }
-};
+}
+
+class Program
+{
+    static void Main()
+    {
+        var rules = new List<IFizzBuzzRule>
+        {             
+            new FizzBuzzRule(),
+            new FizzRule(), 
+            new BuzzRule(),
+        };
+        var calculator = new FizzBuzzCalculator(rules);
+
+        for (int i = 1; i <= 100; i++)
+            Console.WriteLine(calculator.Calculate(i));
+    }
+}
